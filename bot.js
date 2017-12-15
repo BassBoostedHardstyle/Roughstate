e03c63210079b26e1470413b0998a9a9d295239d
qwertyoruiopz <32905436+BassBoostedHardstyle@users.noreply.github.com>
2017-12-15 14:34:37 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+var Discord = require('discord.js');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: MzkxMjEzMzAwMDMxMTYwMzIx.DRVZVA.2EkFH1rPSuUENeJgHNaAU9R7-cw,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, skip, play, help, info, stop, add, search, donate, invite evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !Hi
+            case 'Hi':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hi!, thanx for inviting me to your server! :D'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
